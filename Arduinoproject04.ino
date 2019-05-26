1630efb718fd3ed8f89bb164f3537ddb0002c389
vincenzoraiano <42638331+vincenzoraiano@users.noreply.github.com>
2019-05-26 20:28:52 +0200

    Update Arduinoproject04.ino

--- Arduinoproject04.ino
+++ Arduinoproject04.ino
@@ -8,6 +8,9 @@ int yellow=4;
 //Defininf a variable for the potetiometer.
 int potentiometer;
 
+//Defining the pin of the motor 
+int motor=13;
+
 
 
 
@@ -19,6 +22,8 @@ void setup() {
   pinMode(red,OUTPUT);
   //defining the pinMode.
   pinMode(yellow,OUTPUT);
+  //Definig the engine  pinMode
+  pinMode(motor,OUTPUT);
 
   //open the serial port
   Serial.begin(9600);
@@ -31,25 +36,34 @@ void loop() {
   delay(300);
 
 
+  //This will read the value of the potentiometer.
   Serial.println(potentiometer);
 
 
   //Check statement
   if(potentiometer>=0 && potentiometer <=400){
+    //This is defining how the motor will behave.
+    analogWrite(motor, map(3,0,9,0,255));
     digitalWrite(green,HIGH);
     delay(1000);
     digitalWrite(green,LOW);
     delay(1000);
     digitalWrite(red,LOW);
     digitalWrite(yellow,LOW);
+    
     //Check statement
   }else if(potentiometer>=400 && potentiometer <=800){
+    //This is defining how the motor will behave.
+    analogWrite(motor, map(6,0,9,0,255));
     digitalWrite(yellow,HIGH);
     delay(300);
     digitalWrite(red,LOW);
     digitalWrite(green,LOW);
+    
     //Check statement
   }else if(potentiometer>800){
+    //This is defining how the motor will behave.
+    analogWrite(motor, map(9,0,9,0,255));
     digitalWrite(red,HIGH);
     digitalWrite(yellow, LOW);
     digitalWrite(green,LOW);
